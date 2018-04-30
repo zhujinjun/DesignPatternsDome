0ffb2020eebcd296272187ea6b93ca3593e28225
World Hello <wangyizhujp@126.com>
2018-04-30 21:42:22 +0800

    完成收银系统UI功能

---
+++ 2StrategyPatterns/Form1.Designer.cs
@@ -0,0 +1,182 @@
+﻿namespace _2StrategyPatterns
+{
+    partial class Form
+    {
+        /// <summary>
+        /// 必需的设计器变量。
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// 清理所有正在使用的资源。
+        /// </summary>
+        /// <param name="disposing">如果应释放托管资源，为 true；否则为 false。</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows 窗体设计器生成的代码
+
+        /// <summary>
+        /// 设计器支持所需的方法 - 不要修改
+        /// 使用代码编辑器修改此方法的内容。
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.btnOK = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.labTotalPrice = new System.Windows.Forms.Label();
+            this.btnReset = new System.Windows.Forms.Button();
+            this.txtPrice = new System.Windows.Forms.TextBox();
+            this.txtCount = new System.Windows.Forms.TextBox();
+            this.comCalculationType = new System.Windows.Forms.ComboBox();
+            this.txtDetail = new System.Windows.Forms.TextBox();
+            this.label5 = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // btnOK
+            // 
+            this.btnOK.Location = new System.Drawing.Point(336, 35);
+            this.btnOK.Name = "btnOK";
+            this.btnOK.Size = new System.Drawing.Size(75, 23);
+            this.btnOK.TabIndex = 0;
+            this.btnOK.Text = "确 定";
+            this.btnOK.UseVisualStyleBackColor = true;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(66, 35);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(41, 12);
+            this.label1.TabIndex = 1;
+            this.label1.Text = "单价：";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(66, 80);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(41, 12);
+            this.label2.TabIndex = 2;
+            this.label2.Text = "数量：";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(42, 122);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(65, 12);
+            this.label3.TabIndex = 3;
+            this.label3.Text = "计算方式：";
+            // 
+            // labTotalPrice
+            // 
+            this.labTotalPrice.AutoSize = true;
+            this.labTotalPrice.Font = new System.Drawing.Font("宋体", 14.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(134)));
+            this.labTotalPrice.ForeColor = System.Drawing.Color.Red;
+            this.labTotalPrice.Location = new System.Drawing.Point(66, 298);
+            this.labTotalPrice.Name = "labTotalPrice";
+            this.labTotalPrice.Size = new System.Drawing.Size(69, 19);
+            this.labTotalPrice.TabIndex = 4;
+            this.labTotalPrice.Text = "总计：";
+            // 
+            // btnReset
+            // 
+            this.btnReset.Location = new System.Drawing.Point(336, 80);
+            this.btnReset.Name = "btnReset";
+            this.btnReset.Size = new System.Drawing.Size(75, 23);
+            this.btnReset.TabIndex = 5;
+            this.btnReset.Text = "重 置";
+            this.btnReset.UseVisualStyleBackColor = true;
+            // 
+            // txtPrice
+            // 
+            this.txtPrice.Location = new System.Drawing.Point(124, 35);
+            this.txtPrice.Name = "txtPrice";
+            this.txtPrice.Size = new System.Drawing.Size(182, 21);
+            this.txtPrice.TabIndex = 6;
+            // 
+            // txtCount
+            // 
+            this.txtCount.Location = new System.Drawing.Point(124, 77);
+            this.txtCount.Name = "txtCount";
+            this.txtCount.Size = new System.Drawing.Size(182, 21);
+            this.txtCount.TabIndex = 7;
+            // 
+            // comCalculationType
+            // 
+            this.comCalculationType.FormattingEnabled = true;
+            this.comCalculationType.Items.AddRange(new object[] {
+            "正常计算",
+            "九折计算",
+            "八折计算",
+            "五折计算"});
+            this.comCalculationType.Location = new System.Drawing.Point(124, 122);
+            this.comCalculationType.Name = "comCalculationType";
+            this.comCalculationType.Size = new System.Drawing.Size(182, 20);
+            this.comCalculationType.TabIndex = 8;
+            // 
+            // txtDetail
+            // 
+            this.txtDetail.Location = new System.Drawing.Point(124, 167);
+            this.txtDetail.Multiline = true;
+            this.txtDetail.Name = "txtDetail";
+            this.txtDetail.Size = new System.Drawing.Size(287, 114);
+            this.txtDetail.TabIndex = 1;
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Location = new System.Drawing.Point(66, 167);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(41, 12);
+            this.label5.TabIndex = 9;
+            this.label5.Text = "明细：";
+            // 
+            // Form
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 12F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(503, 351);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.txtDetail);
+            this.Controls.Add(this.comCalculationType);
+            this.Controls.Add(this.txtCount);
+            this.Controls.Add(this.txtPrice);
+            this.Controls.Add(this.btnReset);
+            this.Controls.Add(this.labTotalPrice);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.btnOK);
+            this.Name = "Form";
+            this.Text = "收银系统";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button btnOK;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label labTotalPrice;
+        private System.Windows.Forms.Button btnReset;
+        private System.Windows.Forms.TextBox txtPrice;
+        private System.Windows.Forms.TextBox txtCount;
+        private System.Windows.Forms.ComboBox comCalculationType;
+        private System.Windows.Forms.TextBox txtDetail;
+        private System.Windows.Forms.Label label5;
+    }
+}
+
