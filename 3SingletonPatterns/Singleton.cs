45d0beb302899969ed855b70f8769fde5c93d1dd
朱金平 <wangyizhujp@126.com>
2019-01-15 16:50:49 +0800

    update

---
+++ 3SingletonPatterns/Singleton.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _3SingletonPatterns
+{
+        
+    /// <summary>
+    /// 单例模式
+    /// </summary>
+    public class Singleton
+    {       
+        public static Singleton ingleton;
+        /// <summary>
+        /// private :防止外外部实例化
+        /// </summary>
+        private Singleton()
+        {
+
+        }
+        /// <summary>
+        /// 外部唯一能够访问实例的地方
+        /// </summary>
+        /// <returns></returns>
+        public static Singleton GetSingleton()
+        {
+            if(ingleton == null)
+            {
+                ingleton = new Singleton();
+            }
+            return ingleton;
+        }
+    }
+}
